18124c379b1bb883039290d1363e4b7447c4c40f
hmike96 <hmike1996@gmail.com>
2021-03-22 13:05:36 -0400

    update tinder

--- github-runner-ami/packer/files/source-list-additions.sh
+++ github-runner-ami/packer/files/source-list-additions.sh
@@ -1,4 +1,4 @@
 sudo apt-key add "1646B01B86E50310"
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-key add timber.key
+sudo apt-key add /usr/local/sbin/timber.key
 echo "deb https://repositories.timber.io/public/vector/deb/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/timber.list
