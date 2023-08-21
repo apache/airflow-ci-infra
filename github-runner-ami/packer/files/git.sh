f19bc91ce9dac126369ed4eb7a43fd8418fa5377
Jarek Potiuk <jarek@potiuk.com>
2023-08-21 12:54:47 +0200

    Add back git-lfs and git-ftp and add `--location` to install hub

--- github-runner-ami/packer/files/git.sh
+++ github-runner-ami/packer/files/git.sh
@@ -64,10 +64,21 @@ apt-get update
 apt-get install --upgrade git -y
 git --version
 
+# Install git-lfs
+curl -s $GIT_LFS_REPO/script.deb.sh | bash
+apt-get install -y git-lfs=2.13.3
+
+# Install git-ftp
+apt-get install git-ftp -y
+
+# Remove source repo's
+add-apt-repository --remove $GIT_REPO
+rm /etc/apt/sources.list.d/github_git-lfs.list
+
 #Install hub
 tmp_hub="/tmp/hub"
 mkdir -p "$tmp_hub"
-url=$(curl -s https://api.github.com/repos/github/hub/releases/latest | jq -r '.assets[].browser_download_url | select(contains("hub-linux-amd64"))')
+url=$(curl --location -s https://api.github.com/repos/github/hub/releases/latest | jq -r '.assets[].browser_download_url | select(contains("hub-linux-amd64"))')
 download_with_retries "$url" "$tmp_hub"
 tar xzf "$tmp_hub"/hub-linux-amd64-*.tgz --strip-components 1 -C "$tmp_hub"
 mv "$tmp_hub"/bin/hub /usr/local/bin
